7de5fda10cbb2442351629ca8fba2623234d14ec
Kevin <nait90@gmail.com>
2023-03-21 17:27:22 +0900

    Update ex_bigRecords.js

--- ex_bigRecords.js
+++ ex_bigRecords.js
@@ -1,1 +1,26 @@
-
+const bigRecords = [
+  {
+    name: '홍길동',
+    open: [
+      {
+        kind : "화면오류",
+        num: 101,
+        title: "Title...",
+        item : {
+          // ex_rawData
+          "issue_number": 101,
+          "project_name": "011. 프로젝트 단계",
+          // refer to ex_rawData.js
+          // ...
+          "history_list": [],
+        },
+      }
+    ],
+    inProgress: [],
+    inReview: [],
+    underReview: [],
+    approved: [],
+    reopen: [],
+    issues: [],
+  }
+]
