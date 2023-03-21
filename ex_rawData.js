3e3a40e7827ad1589877a88906b2d02f107f8b3f
Kevin <nait90@gmail.com>
2023-03-21 17:28:03 +0900

    Create ex_rawData.js

---
+++ ex_rawData.js
@@ -0,0 +1,242 @@
+const data = {
+  "proc_status": [
+    {
+      "issue_number": 1234,
+      "project_name": "011. 내부오픈",
+      "biz_type": null,
+      "issue_kind": "화면오류",
+      "environment": "운영",
+      "channel_div": "스타뱅킹",
+      "product_div": "네멋데로 대출",
+      "subject": "[네멋데로 진입시] 맞춰준다 단계에서 핸드폰 정보 다르다고 얼럿 노출",
+      "description": "화면 진입 경로 : ㅁ > b > c>  이런 저런 내용",
+      "current_status": "확인완료",
+      "priority": "Minor",
+      "author": "홍길동",
+      "current_worker": "빌게이츠",
+      "reg_date": "2023-02-24 11:28:48",
+      "end_date": "2023-02-28 15:28:48",
+      "due_date": null,
+      "friendly_limit": null,
+      "fix_worker": "스티브잡스",
+      "fix_date": "2023-02-28 13:28:00",
+      "history_list": [
+        {
+        "change_date": "2023-02-28 13:30:12",
+        "change_user": 22,
+        "note": "이러저러하게 수정함.",
+        "change_item": null,
+        "change_before": null,
+        "change_after": null,
+        "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+      ]
+    },
+    {
+      "issue_number": 354,
+      "project_name": "011. 내부오픈",
+      "biz_type": null,
+      "issue_kind": "화면오류",
+      "environment": "운영",
+      "channel_div": "스타뱅킹",
+      "product_div": "네멋데로 대출",
+      "subject": "[네멋데로] 화면이 정말 안이쁘다",
+      "description": "화면 진입 경로 : 지구 > b > c>  이런 저런 내용",
+      "current_status": "신규",
+      "priority": "Minor",
+      "author": "강감찬",
+      "current_worker": "Ken",
+      "reg_date": "2023-02-24 11:28:48",
+      "end_date": null,
+      "due_date": null,
+      "friendly_limit": null,
+      "fix_worker": null,
+      "fix_date": null,
+      "history_list": [
+        {
+        "change_date": "2023-02-28 13:30:12",
+        "change_user": 22,
+        "note": "이러저러하게 수정함.",
+        "change_item": null,
+        "change_before": null,
+        "change_after": null,
+        "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+        {
+          "change_date": "2023-02-28 13:30:12",
+          "change_user": 22,
+          "note": "이러저러하게 수정함.",
+          "change_item": null,
+          "change_before": null,
+          "change_after": null,
+          "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+        {
+          "change_date": "2023-02-28 13:30:12",
+          "change_user": 22,
+          "note": "이러저러하게 수정함.",
+          "change_item": null,
+          "change_before": null,
+          "change_after": null,
+          "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+      ]
+    },
+    {
+      "issue_number": 387,
+      "project_name": "011. 내부오픈",
+      "biz_type": null,
+      "issue_kind": "화면오류",
+      "environment": "운영",
+      "channel_div": "스타뱅킹",
+      "product_div": "네멋데로 대출",
+      "subject": "[네멋데로] 화면이 정말 안이쁘다",
+      "description": "화면 진입 경로 : 지구 > b > c>  이런 저런 내용",
+      "current_status": "조치중",
+      "priority": "Minor",
+      "author": "강감찬",
+      "current_worker": "Ken",
+      "reg_date": "2023-02-24 11:28:48",
+      "end_date": null,
+      "due_date": null,
+      "friendly_limit": null,
+      "fix_worker": null,
+      "fix_date": null,
+      "history_list": [
+        {
+        "change_date": "2023-02-28 13:30:12",
+        "change_user": 22,
+        "note": "이러저러하게 수정함.",
+        "change_item": null,
+        "change_before": null,
+        "change_after": null,
+        "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+        {
+          "change_date": "2023-02-28 13:30:12",
+          "change_user": 22,
+          "note": "이러저러하게 수정함.",
+          "change_item": null,
+          "change_before": null,
+          "change_after": null,
+          "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+        {
+          "change_date": "2023-02-28 13:30:12",
+          "change_user": 22,
+          "note": "이러저러하게 수정함.",
+          "change_item": null,
+          "change_before": null,
+          "change_after": null,
+          "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+      ]
+    },
+    {
+      "issue_number": 400,
+      "project_name": "011. 내부오픈",
+      "biz_type": null,
+      "issue_kind": "개선사항",
+      "environment": "운영",
+      "channel_div": "스타뱅킹",
+      "product_div": "네멋데로 대출",
+      "subject": "잘좀하자",
+      "description": "화면 진입 경로 : 지구 > b > c>  이런 저런 내용",
+      "current_status": "신규",
+      "priority": "Minor",
+      "author": "강감찬",
+      "current_worker": "Ken",
+      "reg_date": "2023-02-24 11:28:48",
+      "end_date": null,
+      "due_date": null,
+      "friendly_limit": null,
+      "fix_worker": null,
+      "fix_date": null,
+      "history_list": [
+        {
+        "change_date": "2023-02-28 13:30:12",
+        "change_user": 22,
+        "note": "이러저러하게 수정함.",
+        "change_item": null,
+        "change_before": null,
+        "change_after": null,
+        "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+        {
+          "change_date": "2023-03-17 13:30:12",
+          "change_user": 22,
+          "note": "이러저러하게 수정함.",
+          "change_item": null,
+          "change_before": null,
+          "change_after": null,
+          "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+        {
+          "change_date": "2023-02-28 13:30:12",
+          "change_user": 22,
+          "note": "이러저러하게 수정함.",
+          "change_item": null,
+          "change_before": null,
+          "change_after": null,
+          "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+      ]
+    },
+    {
+      "issue_number": 623,
+      "project_name": "011. 내부오픈",
+      "biz_type": null,
+      "issue_kind": "화면오류",
+      "environment": "운영",
+      "channel_div": "스타뱅킹",
+      "product_div": "네멋데로 대출",
+      "subject": "[네멋데로] 화면이 정말 안이쁘다",
+      "description": "화면 진입 경로 : 지구 > b > c>  이런 저런 내용",
+      "current_status": "조치중",
+      "priority": "Minor",
+      "author": "강감찬",
+      "current_worker": "Ken",
+      "reg_date": "2023-03-12 11:28:48",
+      "end_date": null,
+      "due_date": null,
+      "friendly_limit": null,
+      "fix_worker": null,
+      "fix_date": null,
+      "history_list": [
+        {
+        "change_date": "2023-02-28 13:30:12",
+        "change_user": 22,
+        "note": "이러저러하게 수정함.",
+        "change_item": null,
+        "change_before": null,
+        "change_after": null,
+        "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+        {
+          "change_date": "2023-02-28 13:30:12",
+          "change_user": 22,
+          "note": "이러저러하게 수정함.",
+          "change_item": null,
+          "change_before": null,
+          "change_after": null,
+          "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+        {
+          "change_date": "2023-02-28 13:30:12",
+          "change_user": 22,
+          "note": "이러저러하게 수정함.",
+          "change_item": null,
+          "change_before": null,
+          "change_after": null,
+          "tranlated": "사용자 [홍길동] 이(가) 노트 정보를 기록함."
+        },
+      ]
+    },
+  ]
+}
+
+exports.data = data
+
+// module.exports = {
+//   data,
+// }
