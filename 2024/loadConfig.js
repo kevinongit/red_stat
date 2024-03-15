1564f873166eab9052976c6c65f5415b11572804
Kevin <nait90@gmail.com>
2024-03-15 14:35:25 +0900

    Update loadConfig.js

--- 2024/loadConfig.js
+++ 2024/loadConfig.js
@@ -5,18 +5,22 @@ const file = './server.cfg'
 
 function getConfig() {
   let config = {}
+  
   return new Promise((resolve) => {
     const rl = readline.createInterface({
       input: fs.createReadStream(file),
     })
+    
     rl.on('line', function(faw) {
       const line = raw.split('#')[0].trim()
       const [key, value] = line.split('=')
       if (!key || !value) {
         return;
       }
-      config[key] = (key === 'YIELD_MEMBER') ? value.split(/,\s+/) : value
+      config[key] = (key === 'YIELD_MEMBER') ? value.split(/,\s+/) : value;
+      // console.log(`'${key}' : '${value}'`)
     })
+    
     rl.on('close', () => {
       resolve({
         redmineHost: config.REDMINE_HOST,
