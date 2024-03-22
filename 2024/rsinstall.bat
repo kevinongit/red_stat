06141d8845c74de88994b8afe2f6105ecd41d0f0
Kevin <nait90@gmail.com>
2024-03-22 14:17:22 +0900

    Update rsinstall.bat

--- 2024/rsinstall.bat
+++ 2024/rsinstall.bat
@@ -1,1 +1,7 @@
-
+xcopy main.js D:\Space\rstat2023\ /Y
+xcopy rs-style.css D:\Space\rstat2023\ /Y
+xcopy rs-template.html D:\Space\rstat2023\ /Y
+xcopy rs-script.js D:\Space\rstat2023\ /Y
+xcopy server.js D:\Space\rstat2023\ /Y
+xcopy server.cfg D:\Space\rstat2023\ /Y
+xcopy loadConfig.js D:\Space\rstat2023\ /Y
