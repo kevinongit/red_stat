fb8dd36ea4ae95241a7d32fd74d4d7cfd320add2
Kevin <nait90@gmail.com>
2024-04-10 20:42:10 +0900

    typo

--- 2024/server.js
+++ 2024/server.js
@@ -214,7 +214,7 @@ function html_page(host, reqUrl, dirs) {
     <head>
       <meta charset='utf-8'>
       <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-      <title>Listing of ${reqIrl}</title>
+      <title>Listing of ${reqUrl}</title>
       <meta name='viewport' content='width=device-width, initial-scale=1'>
       <!-- link rel='stylesheet' type='text/css' media='screen' href='../commonStyle.css' -->
       <!-- script src='main.js'></script -->
